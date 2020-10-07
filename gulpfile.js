75c9f6257f749f65ac0f170fcab26191bde4bce9
bogdangill <herecomesthefog@gmail.com>
2020-10-07 16:24:45 +0500

    function to concat js files added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
+const concat = require('gulp-concat');
 
 function convertLess() {
     return gulp.src('./src/less/main.less')
@@ -7,4 +8,13 @@ function convertLess() {
         .pipe(gulp.dest('./dist/css'))
 }
 
+function createBundle() {
+    return gulp.src(['./src/js/*.js', './src/blocks/**/*.js'])
+        .on('data', function(file){ console.log(file);})
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('./dist/js'));
+}
+
+
 exports.convertLess = convertLess;
+exports.createBundle = createBundle;
